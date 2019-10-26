ecd208833c48e45fa4c1e96569b362b166352952
aady501 <57033699+aady501@users.noreply.github.com>
2019-10-26 15:18:18 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hit_me')
+def hit_me():
+    arguments = request.args.to_dict()
+    return render_template('hit_me.html', firstName=arguments['firstName'], lastName=arguments['lastName'])
+
+
+@app.route('/')
+def index():
+    return 'Deployed Flask project on Heroku by ~aady95'
+
+
+app.run('127.0.0.1',port=8080, debug=True)
